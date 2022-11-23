cbe01dc7756702e17f85851cb47da3f877caf814
avi-nakai <avir@nakairobotics.com>
2022-11-23 16:42:03 +0200

    Remove ignored files

--- lian/build/lian_nav2/CMakeFiles/lian_nav2_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/lian_nav2_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/lian_nav2_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
