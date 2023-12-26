09db525fca91de219606cd23e0e3ab2ad2d0bcdd
Korinin38 <artkash@bk.ru>
2023-12-26 19:13:09 +0300

    fix: command line arguments processing

--- src/tech_debt/main.cpp
+++ src/tech_debt/main.cpp
@@ -3,10 +3,10 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-    // if (argc != 2) {
-    //     return 1;
-    // }
-    Mission Mission("../examples/eLianTestOld.xml");
+     if (argc != 2) {
+         return 1;
+     }
+    Mission Mission(argv[1]);
 
     Mission.createSearch();
     Mission.createLog();
